9f77b48c225797739d0b669b590556500c934966
Jules Lambert <juleslambert345@hotmail.com>
2024-01-28 10:21:51 -0500

    update the setup file for pylint

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+"""
+This is a setup script for 'weather'.
+
+This script is used to install, build, and distribute 'weather'.
+It defines package metadata and other configuration relevant to the package.
+"""
+
 from setuptools import setup
 
 setup(
