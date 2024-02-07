f8abb6145a39136a47480b7284aea6d400673e54
jules@vyoo.ai <juleslambert345@hotmail.com>
2024-02-07 16:53:08 -0500

    debuging thing

--- client_app/app.py
+++ client_app/app.py
@@ -17,6 +17,7 @@ model_folder = Path(__file__).resolve().parent.parent / "models"
 
 model = joblib.load(model_folder / 'model.pkl')
 input_transformer = joblib.load(model_folder / 'predictors_feature_eng_pipeline.pkl')
+data_ingestion_pipeline = joblib.load(model_folder / 'dataset_ingestion_pipeline.pkl')
 
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
@@ -30,11 +31,18 @@ file_path = 'phones.txt'
 with open(file_path, 'r') as file:
     phones = file.readlines()
 
+
+useless_columns = ['S_No', 'Location', 'Apparent_temperature']
+
 # Create a POST endpoint to receive JSON data and return a response
 @app.post("/predict/")
 async def predict(item: Item):
     df = pd.DataFrame([item.dict()])
     df.rename(columns=json_to_dataframe_col, inplace=True)
+    for column_name in useless_columns:
+        df[column_name] = ''
+    df = data_ingestion_pipeline.transform(df)
+    print(df)
     y = model.predict(input_transformer.transform(df))
     if y[0]==1:
         for phone in phones:
